37fb24a01ac38813b0e4b07c44e4c6be8bfc679f
kinderGartenDemos <leiw4github@gmail.com>
2017-01-27 22:45:18 +0100

    delete useless files + gitignore

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,5 @@
-//import {Person} from './Person.js'
 import {Weather} from './weather/Weather.js'
 
-// var person = new Person('lei');
-// person.sayHi();
 var city;
 var weather = new Weather('bot');
 $(document).ready(function(){
